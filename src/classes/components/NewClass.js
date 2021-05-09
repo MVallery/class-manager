24596a4d5f377cf1ac23bdfd610ce77112164de0
Melissa Vallery <mvallery88@gmail.com>
2021-05-08 22:21:08 -0500

    change database request call

--- src/classes/components/NewClass.js
+++ src/classes/components/NewClass.js
@@ -74,7 +74,7 @@ const NewClass = (props) => {
   
     tempClassList.push(tempClass);
     try {
-     sendRequest('http://localhost:5000/api/users/'+props.userId+'/create-class', "POST", 
+     sendRequest('/api/users/'+props.userId+'/create-class', "POST", 
       JSON.stringify({
         title: tempClass.title,
         students: tempClass.students,
